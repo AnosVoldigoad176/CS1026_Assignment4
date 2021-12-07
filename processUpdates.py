70c1d55035fdda9744cfbe6ec4db2e560eec5572
HarryVu176 <harry@moonivia.com>
2021-12-06 20:37:41 -0500

    I guess?

--- processUpdates.py
+++ processUpdates.py
@@ -1,41 +1,164 @@
+import catalogue
 from country import Country
 from catalogue import CountryCatalogue
+import os.path
 
 
-def processUpdates(countryFileName, updateFileName, badUpdateFile):
+def countryValidator(string):
+    """
+    This function check if the country name is in valid form (no space, in upper case first letter,
+    no special characters except for "_" and not space characters)
+    :param string: Input string
+    :return: True if valid, False if not
+    """
+    string = string.strip()
+    if len(string) == 0:
+        return False
+    if all(c.isalpha() or c == '_' for c in string) and string[0].isupper():
+        return True
+    return False
 
+
+def populationValidator(string):
+    """
+    This function check if the population or area is in valid form (no space, "," separator)
+    :param string: Input string
+    :return: True if valid, False if not
+    """
+    if string.strip().startswith("P="):
+        originalp = string.strip().replace("P=", '').replace(' ', '')
+    else:
+        return False
+
+    temporary = originalp.replace(',', '')
+    temporary = '{:,}'.format(int(temporary))
+    if originalp == temporary:
+        return True
+    return False
+
+
+def areaValidator(string):
+    """
+    This function check if the area is in valid form (no space, "," separator)
+    :param string: Input string
+    :return: True if valid, False if not
+    """
+    if string.strip().startswith("A="):
+        originala = string.strip().replace("A=", '').replace(' ', '')
+    else:
+        return False
+
+    temporary = originala.replace(',', '')
+    temporary = '{:,}'.format(int(temporary))
+    if originala == temporary:
+        return True
+    return False
+
+
+def continentValidator(string):
+    """
+    This function check if the continent name is in valid form
+    :param string: Input String
+    :return: True if valid, False if not
+    """
+    validContinent = ["Africa", "Antarctica", "Arctic", "Asia", "Europe", "North_America", "South_America"]
+    if string.strip().startswith("C="):
+        continent = string.strip().replace("C=", '')
+        if continent in validContinent:
+            return True
+    return False
+
+
+def processUpdates(countryFileName, updateFileName, badUpdateFileName):
     """
     This function take original data file (file 1), update it based on the file containing what to update (file 2)
     and store it into "output.txt". For invalid records, store it to file specified in third param.
     :param countryFileName: Original data file
     :param updateFileName: File containing what to update
-    :param badUpdateFile: Store invalid update records
-    :return:
+    :param badUpdateFileName: Store invalid update records
+    :return: (True, CatalogName) if successful, (False, None) if not
     """
 
-    validContinent = ["Africa", "Antarctica", "Arctic", "Asia", "Europe", "North_America", "South_America"]
+    # Check if country file exists if no then prompt user to enter file name until it exists
+    while not os.path.isfile(countryFileName):
+        choice1 = input("Country file doesn't exist. Do you want to quit? (y/n): ")
+        if choice1.lower() == "n":
+            countryFileName = input("Re-enter file name: ")
+        else:
+            open("output.txt", "w").write("Update Unsuccessful\n")
+            return False, None
 
-    try:
-        with open(updateFileName, 'r') as updateFile:
-            with open(badUpdateFile, 'w') as badUpdateFile:
-                with open(countryFileName, 'r') as countryFile:
-                    # Go through the whole file
-                    for line in updateFile:
-                        # Make sure there is no trailing space and split the line by ";"
-                        lineListFormat = line.strip().split(";")
-                        # Check if the country name is valid
-                        if lineListFormat[0].isupper() and ' ' not in line[0]:
-                            countryName = line[0]
-                            # Get length of lineListFormat and loop through index
-                            for i in range(1, len(lineListFormat)):
-                                if lineListFormat[i].startswith("P="):
-                                    originalPopulation = lineListFormat[i].strip().replace("P=", '')
-                                    newPopulation = originalPopulation.replace(',', '')
+    # Check if update file exists if no then prompt user to enter file name until it exists
+    while not os.path.isfile(updateFileName):
+        choice2 = input("Update file doesn't exist. Do you want to quit? (y/n): ")
+        if choice2.lower() == "n":
+            updateFileName = input("Re-enter file name: ")
+        else:
+            open("output.txt", "w").write("Update Unsuccessful\n")
+            return False, None
 
-                        else:
-                            countryNameInvalid = line[0]
+    updateFile = open(updateFileName, "r")
+    badUpdateFile = open(badUpdateFileName, "w")
 
-    except FileNotFoundError:
-        print("File not found")
+    # Initialize country catalogue
+    countryCat = CountryCatalogue(countryFileName)
 
-            
+    population = ""
+    area = ""
+    continent = ""
+
+    # Process country file using catalogue
+    for line in updateFile:
+        # Ensure no trailing spaces then split word in line by ";"
+        lineListFormat = line.strip().split(";")
+        # Check if country is valid (no space, in upper case first letter)
+        country = lineListFormat[0].strip()
+        if len(lineListFormat) > 4:
+            badUpdateFile.write(line)
+            continue
+        if countryValidator(country):
+            # Check if population, area and continent format is valid
+            for element in lineListFormat[1:]:
+                if element.strip().startswith("P=") and population == "":
+                    if populationValidator(element):
+                        population = element.replace("P=", "").strip()
+                    else:
+                        badUpdateFile.write(line)
+                        break
+                elif element.strip().startswith("A=") and area == "":
+                    if areaValidator(element):
+                        area = element.replace("A=", "").strip()
+                    else:
+                        badUpdateFile.write(line)
+                        break
+                elif element.strip().startswith("C=") and continent == "":
+                    if continentValidator(element):
+                        continent = element.replace("C=", "").strip()
+                    else:
+                        badUpdateFile.write(line)
+                        break
+                else:
+                    badUpdateFile.write(line)
+                    break
+            # Create country object
+            countryObj = Country(country, population, area, continent)
+            print(countryObj)
+            if population != "":
+                countryCat.setPopulationOfCountry(countryObj, population)
+                population = ""
+            if area != "":
+                countryCat.setAreaOfCountry(countryObj, area)
+                area = ""
+            if continent != "":
+                countryCat.setContinentOfCountry(countryObj, continent)
+                continent = ""
+        else:
+            badUpdateFile.write(line)
+            continue
+
+    # Output file for successful updates
+    countryCat.saveCountryCatalogue("output.txt")
+    badUpdateFile.close()
+    updateFile.close()
+    return True, countryCat
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import processUpdates as pu
+
+
+print(pu.countryValidator("Sweden"))
+print(pu.populationValidator("P=9,995,345"))
+print(pu.areaValidator("A=450,295"))
+print(pu.continentValidator("C=Europe"))
+
